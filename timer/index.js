130e4dc2e245ee80ab62848c8903e04494020d73
Alberto Chirita <alberto.marian.chirita@gmail.com>
2022-03-22 18:23:31 +0200

    Day 27

--- timer/index.js
+++ timer/index.js
@@ -1,29 +1,22 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener("click", this.start);
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-
-  start = () => {
-    console.log(this);
-  };
-
-  pause() {
-    console.log("Pause!");
-  }
-
-  onDurationChange() {}
-
-  tick() {}
-}
-
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector("#circle");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
-timer.start();
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+circle.setAttribute("stroke-dasharray", perimeter);
+
+let currentOffset = 0;
+
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer started!");
+  },
+  onTick() {
+    circle.setAttribute("stroke-dashoffset", currentOffset);
+    currentOffset = currentOffset - 1;
+  },
+  onComplete() {
+    console.log("Timer is completed!");
+  },
+});
